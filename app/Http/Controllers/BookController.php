c6faac035b9aa5c60b71e8b13806f0110828252f
David Winch <d.winchy+git@gmail.com>
2023-08-19 17:37:31 +0100

    Used model creation instead of raw SQL

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,16 +11,32 @@ class BookController extends Controller
     public function addBookForm() {
         return view('book_create');
      }
+
+     public function bookExists($isbn){
+        $count = Book::where('isbn', $isbn)
+            ->count();
+        return $count > 0; 
+     }
       
      public function insert(Request $request) {
-        $isbn = $request->input('isbn');
-        $title = $request->input('title');
-        $fk_category_id = $request->input('category_id');
-        $published_date = $request->input('published_date');
-        $created_at = date("Y-m-d H:i:s");
-        $updated_at = date("Y-m-d H:i:s");
-        DB::insert('insert into books (isbn, title, fk_category_id, published_date, created_at, updated_at) values(?, ?, ?, ?, ?, ?)',
-            [$isbn, $title, $fk_category_id, $published_date, $created_at, $updated_at]);
+
+        if ($this->bookExists($request->input('isbn'))){
+            echo "Book with isbn '" . $request->input('isbn') . "' already exists.<br/>";
+            echo '<a href = "/add-book">Click Here</a> to go back.';
+            return;
+        }
+
+        $book = Book::create([
+            'isbn' => $request->input('isbn'),
+            'title' => $request->input('title'),
+            'fk_category_id' => $request->input('category_id'),
+            'published_date' => $request->input('published_date'),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+        
+        $book->save();
+
         echo "Record inserted successfully.<br/>";
         echo '<a href = "/add-book">Click Here</a> to go back.';
      }
--- app/Http/Controllers/BookLoanController.php
+++ app/Http/Controllers/BookLoanController.php
@@ -8,33 +8,39 @@ use Illuminate\Support\Facades\DB;
 
 class BookLoanController extends Controller
 {
-    public function addBookLoanForm() {
+    public function addBookLoanForm()
+    {
         return view('book_loan_create');
-     }
+    }
 
-     public function getLoanedBookCount($member_id) {
+    public function getLoanedBookCount($member_id)
+    {
         $count = BookLoan::where('fk_member_id', $member_id)
             ->whereNull('return_date')
             ->count();
         return $count;
-     }
-      
-     public function insert(Request $request) {
-        $fk_member_id = $request->input('member_id');
-        $fk_stock_id = $request->input('stock_id');
-        $loan_date = $request->input('loan_date');
-        $created_at = date("Y-m-d H:i:s");
-        $updated_at = date("Y-m-d H:i:s");
+    }
 
-        if ($this->getLoanedBookCount($fk_member_id) >= 5) {
-            echo "This user currently has 5 books on loan. This book cannot be taken until one is returned.<br/>";
+    public function insert(Request $request)
+    {
+
+        if ($this->getLoanedBookCount($request->input('member_id')) >= 5) {
+            echo "User " . $request->input('fk_member_id') . " currently has 5 books on loan. This book cannot be taken until one is returned.<br/>";
             echo '<a href = "/loan-book">Click Here</a> to go back.';
             return;
         }
 
-        DB::insert('insert into book_loans (fk_member_id, fk_stock_id, loan_date, created_at, updated_at) values(?, ?, ?, ?, ?)',
-            [$fk_member_id, $fk_stock_id, $loan_date, $created_at, $updated_at]);
+        $bookLoan = BookLoan::create([
+            'fk_member_id' => $request->input('member_id'),
+            'fk_stock_id' => $request->input('stock_id'),
+            'loan_date' => $request->input('loan_date'),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        $bookLoan->save();
+
         echo "Record inserted successfully.<br/>";
         echo '<a href = "/loan-book">Click Here</a> to go back.';
-     }
+    }
 }
